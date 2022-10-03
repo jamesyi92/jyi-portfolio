a354a6cafb15a38308252f06a421c27bb533a458
James Yi <james.yi@visier.com>
2022-10-03 16:18:48 -0700

    .

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,34 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { ThemeProvider } from 'styled-components';
-import lightTheme from '../utils/themes/lightTheme';
-import darkTheme from '../utils/themes/darkTheme';
-import Header from '../components/Header';
-import ThemeSwitcher from './ThemeSwitcher';
+import React, { useState, useEffect } from "react"
+import PropTypes from "prop-types"
+import { ThemeProvider } from "styled-components"
+import lightTheme from "../utils/themes/lightTheme"
+import darkTheme from "../utils/themes/darkTheme"
+import Header from "../components/Header"
+import ThemeSwitcher from "./ThemeSwitcher"
 
-import '../scss/main.scss';
+import "../scss/main.scss"
 
 const Layout = ({ children }) => {
+  useEffect(() => {
+    const stored = window.localStorage.getItem("isDarkMode")
+  })
+  const [isDarkMode, setIsDarkMode] = useState(stored === "true" ? true : false)
 
-	const stored = window.localStorage.getItem('isDarkMode');
-	const [isDarkMode, setIsDarkMode] = useState(stored === 'true' ? true : false)
-
-	return (
-	  <ThemeProvider theme={ isDarkMode ? darkTheme : lightTheme }>
-	    <React.Fragment>
-	      <Header />
-	      <ThemeSwitcher 
-	      	onClick={() => {
-		      	setIsDarkMode(!isDarkMode);
-		      	window.localStorage.setItem('isDarkMode', !isDarkMode);
-		      }}
-	      >
-	      	Toggle Theme
-	      </ThemeSwitcher>
-	      <main>
-	      	{children}
-	      </main>
-	    </React.Fragment>
-	  </ThemeProvider>
+  return (
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <React.Fragment>
+        <Header />
+        <ThemeSwitcher
+          onClick={() => {
+            setIsDarkMode(!isDarkMode)
+            window.localStorage.setItem("isDarkMode", !isDarkMode)
+          }}
+        >
+          Toggle Theme
+        </ThemeSwitcher>
+        <main>{children}</main>
+      </React.Fragment>
+    </ThemeProvider>
   )
 }
 
